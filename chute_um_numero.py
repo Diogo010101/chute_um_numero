4b7bcdbe0e5821f16279037749d71bc4ba60fa1b
Diogo Jose <diogospk@hotmail.com>
2022-01-26 19:19:28 -0300

    correção

--- chute_um_numero.py
+++ chute_um_numero.py
@@ -1,14 +1,16 @@
-#Chute um número de 0 a 1000 / Projeto para fins acadêmicos 
+#Chute um número de 0 a 1000 / Projeto para fins acadêmicos
 import random
 x=random.randint(0,1000)
 resposta=int(input("Digite um número entre 0 a 1000: "))
 while resposta !=x:
-    if resposta<x:
-        print("\nTente um número menor...")
-    elif resposta>x:
+    if resposta ==44 and x <44:
+        print("Essa é a resposta para vida o universo e tudo mais, tente um número menor ")    
+    elif resposta ==44 and x >44:
+        print("Essa é a resposta para vida o universo e tudo mais, tente um número maior ")  
+    elif resposta < x :
         print("\nTente um número maior...")
-    elif resposta<0 or resposta>1000:
-        print("Escolha um número entre 0 e 1000")
+    elif resposta >x:
+        print("\nTente um número menor...")      
     resposta=int(input("Digite um número entre 0 a 1000: "))
-else:
-    print("Você acertou!")        
+
+print("Você acertou!")    
